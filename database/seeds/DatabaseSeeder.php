74cbc753cbf850ca5f0ecaefd860050ca6d97f52
Jake Vivian <jacob@sadsoft.com>
2016-01-20 07:26:31 +0000

    Fixed more issues with seeds (forgot to add User seed)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ShiftSeederTable::class);
+        // Turn off FK checks so we can work with our data and not get errors
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        // Generate Users
+        $this->call(UserTableSeeder::class);
+        $this->command->info('User table seeded!');
+        // Generate Shifts
+        $this->call(ShiftTableSeeder::class);
         $this->command->info('Shift table seeded!');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\User;
 class UserTableSeeder extends Seeder
 {
     /**
@@ -11,6 +11,39 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $users = [
+            1 => [
+                'name' => 'John Employee',
+                'role' => 'employee',
+                'email' => 'employee1@example.com',
+                'password' => bcrypt('demo'),
+                'phone'    => '555-555-5551',
+            ],
+            2 => [
+                'name' => 'Joe Manager',
+                'role' => 'manager',
+                'email' => 'manager@example.com',
+                'password' => bcrypt('demo'),
+                'phone'    => '555-555-5552',
+            ],
+            3 => [
+                'name' => 'Jake Employee',
+                'role' => 'employee',
+                'email' => 'employee2@example.com',
+                'password' => bcrypt('demo'),
+                'phone'    => '555-555-5553',
+            ],
+        ];
+
+        // Remove all old seed data
+        DB::table('users')->delete();
+
+        // Create Fake users
+        foreach($users as $key => $user) {
+            User::create(
+                $user
+            );
+        }
     }
 }
